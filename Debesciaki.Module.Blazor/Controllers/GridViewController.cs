236879d935a10da01101dab88c532b662d8bf1e6
Patryk Wyprzał <pwyprzal@gabos.pl>
2021-12-12 16:13:28 +0100

    GridViewController

---
+++ Debesciaki.Module.Blazor/Controllers/GridViewController.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.Blazor;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Blazor.Editors.Grid;
+
+namespace Debesciaki.Module.Blazor.Controllers
+{
+    public class GridViewController : ViewController<ListView>
+    {
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            if (View.Editor is GridListEditor gridListEditor)
+            {
+                IDxDataGridAdapter dataGridAdapter = gridListEditor.GetDataGridAdapter();
+                dataGridAdapter.DataGridModel.CssClass += " table-striped";
+
+                dataGridAdapter.DataGridModel.ColumnResizeMode = DataGridColumnResizeMode.Component;
+                dataGridAdapter.DataGridModel.ShowFilterRow = true;
+                foreach (var column in gridListEditor.Columns)
+                {
+                    if (column.Width < 150)
+                    {
+                        column.Width = 150;
+                    }
+                }
+            }
+        }
+    }
+}
