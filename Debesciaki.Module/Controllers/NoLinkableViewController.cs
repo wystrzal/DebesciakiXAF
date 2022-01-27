4f07335eaccadeafa180f3a84d84709e01fcc9a4
Patryk Wyprzał <pwyprzal@gabos.pl>
2022-01-27 23:50:28 +0100

    Add NoLinkableViewController

---
+++ Debesciaki.Module/Controllers/NoLinkableViewController.cs
@@ -0,0 +1,33 @@
+﻿using Debesciaki.Module.Interfaces;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.SystemModule;
+
+namespace Debesciaki.Module.Controllers
+{
+    public class NoLinkableViewController : ObjectViewController<ObjectView, INoLinkable>
+    {
+        private const string linkUnlinkAction = "LinkUnlinkAction";
+        private LinkUnlinkController linkUnlinkController;
+
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            linkUnlinkController = Frame.GetController<LinkUnlinkController>();
+            if (linkUnlinkController != null)
+            {
+                linkUnlinkController.LinkAction.Active.SetItemValue(linkUnlinkAction, false);
+                linkUnlinkController.UnlinkAction.Active.SetItemValue(linkUnlinkAction, false);
+            }
+        }
+
+        protected override void OnDeactivated()
+        {
+            if (linkUnlinkController != null)
+            {
+                linkUnlinkController.LinkAction.Active.RemoveItem(linkUnlinkAction);
+                linkUnlinkController.UnlinkAction.Active.RemoveItem(linkUnlinkAction);
+            }
+            base.OnDeactivated();
+        }
+    }
+}
--- Debesciaki.Module/Interfaces/IColor.cs
+++ Debesciaki.Module/Interfaces/IColor.cs
@@ -1,6 +1,6 @@
 ﻿using System.Drawing;
 
-namespace Debesciaki.Module.BusinessObjects
+namespace Debesciaki.Module.Interfaces
 {
     public interface IColor
     {
